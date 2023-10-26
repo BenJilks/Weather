6a8cf2f6102ba72f70ff9505d9c408506fdd8024
BenJilks <benjyjilks@gmail.com>
2023-10-26 12:24:48 +0100

    Add list of locations.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { useState } from 'react'
 import LocationCard from './LocationCard.tsx'
+import style from './App.module.css'
 
 function App() {
+    const [locations, set_locations] = useState(['London', 'Paris'])
+
     return (
-        <LocationCard location='London' />
+        <div className={ style.location_list }>
+            { locations.map(location => <LocationCard location={ location } />) }
+        </div>
     )
 }
 
