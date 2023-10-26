14094e4f09118e51147a54a4f951ab153cae3226
BenJilks <benjyjilks@gmail.com>
2023-10-26 12:21:16 +0100

    Allow changing location.

--- src/LocationCard.tsx
+++ src/LocationCard.tsx
@@ -1,5 +1,5 @@
 import { Fragment, useEffect, useState } from 'react'
-import { Card, Typography, Button } from '@mui/joy'
+import { Card, Typography, Button, Autocomplete } from '@mui/joy'
 import style from './LocationCard.module.css'
 
 import HourCard from './HourCard.tsx'
@@ -10,19 +10,24 @@ interface Params {
     location: string,
 }
 
-export default function LocationCard({ location }: Params) {
+export default function LocationCard(params: Params) {
+    const [location, set_location] = useState(params.location)
+
     const [forecast_response, set_forecast_response] = useState(null as null | ForecastResponse)
     const forecast = forecast_response?.forecast
     const day = forecast?.forecastday?.[0]
     const current = forecast_response?.current
     useEffect(() => {
         weather_api.forecast({ location }).then(set_forecast_response)
-    }, [])
+    }, [location])
 
     const on_remove = () => {
     }
 
-    const on_edit = () => {
+    const on_location_change = (value: string | null) => {
+        if (value) {
+            set_location(value)
+        }
     }
 
     return (
@@ -30,8 +35,12 @@ export default function LocationCard({ location }: Params) {
             <div className={ style.location }>
                 <div className={ style.day_info }>
                     <Button className={ style.remove } onClick={ on_remove } variant="plain">Remove</Button>
-                    <Typography className={ style.location_name } level='h3'>{ location }</Typography>
-                    <Button className={ style.edit } onClick={ on_edit } variant="plain">E</Button>
+                    <Autocomplete
+                        className={ style.location_name }
+                        options={ ['London', 'Paris'] }
+                        value={ location }
+                        onChange={ (_, value) => on_location_change(value) }
+                    />
                     <Typography className={ style.tempreture } level='body-md'>{ current?.temp_c }°C</Typography>
                 </div>
 
